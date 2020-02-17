4c30f4a9dddee5bab0e39cff2e91e87780d0c7f3
Nina Zakharenko <nzakharenko@gmail.com>
2020-02-17 15:22:48 -0800

    Initial implementation of menu states

--- code.py
+++ code.py
@@ -1,8 +1,67 @@
 from adafruit_pybadger import PyBadger
+import time
+
+
+class Action():
+
+    def display(self):
+        # TODO NZ: clear display here?
+        raise NotImplementedError
+
+    def handle_event(self):
+        raise NotImplementedError
+
+
+class PressStart(Action):
+
+    def display(self, pybadger):
+        pybadger.show_business_card(
+            image_name="images/initial.bmp", email_string_one="press start to begin"
+        )
+
+    def handle_event(self, pybadger):
+        if pybadger.button.start:
+            print("Start pressed!")
+            menu.change_state(BadgeMenu.MENU)
+
+
+class Menu(Action):
+
+    def display(self, pybadger):
+        print("Display Menu Here")
+
+    def handle_event(self, pybadger):
+        if pybadger.button.a:
+            print("Button A pressed")
+
+
+class BadgeMenu():
+    START = PressStart()
+    MENU = Menu()
+    # NAME_BADGE = 2
+    # QR_CODE = 3
+    # EXPOVERT = 4
+    # THANKS = 5
+    # EASTER_EGG = 6
+
+    current_state = START
+
+    def __init__(self, pybadger):
+        self.pybadger = pybadger
+        self.current_state.display(self.pybadger)
+
+    def check_event(self):
+        self.current_state.handle_event(self.pybadger)
+
+    def change_state(self, new_state):
+        print("Changing state to", new_state.__class__)
+        self.current_state = new_state
 
 
 pybadger = PyBadger()
-pybadger.show_business_card(image_name="images/initial.bmp", email_string_one="press start to begin")
+menu = BadgeMenu(pybadger)
+
 
 while True:
-    pass
+    menu.check_event()
+    time.sleep(0.5)  # Debounce
