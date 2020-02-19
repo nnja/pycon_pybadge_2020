9e7b4ebe20c30c7d66ee1918a96b2c0d361dbf5b
Nina Zakharenko <nzakharenko@gmail.com>
2020-02-19 14:06:28 -0800

    Move QR code generation to a util module

--- code.py
+++ code.py
@@ -3,8 +3,8 @@ import time
 import displayio
 from adafruit_button import Button
 import terminalio
-import adafruit_miniqr
 from adafruit_display_text import label
+from util import generate_qr_code_display_group
 
 
 class State():
@@ -115,36 +115,9 @@ class NameBadge(DefaultMenuItemState):
 
 class QrCode(DefaultMenuItemState):
 
-    def display(self, pybadger, url = "https://aka.ms/pycon2020"):
-        qr_code = adafruit_miniqr.QRCode(qr_type=2)
-        qr_code.add_data(bytearray(url))
-        qr_code.make()
-
-        palette = displayio.Palette(2)
-        palette[0] = 0xFFFFFF
-        palette[1] = 0x000000
-
-        qr_bitmap = pybadger.bitmap_qr(qr_code.matrix)
-        qr_img = displayio.TileGrid(
-            qr_bitmap,
-            pixel_shader=palette,
-            x=int(qr_bitmap.width / 2),
-            y=2,
-        )
-
-        qr_code = displayio.Group(scale=3)
-        qr_code.append(qr_img)
-
-        qr_and_text_group = displayio.Group()
-        qr_and_text_group.append(qr_code)
-        qr_and_text_group.append(
-            pybadger._create_label_group(
-                text=url,
-                font=terminalio.FONT,
-                scale=1,
-                height_adjustment=0.84)
-        )
-        pybadger.display.show(qr_and_text_group)
+    def display(self, pybadger, url="https://aka.ms/pycon2020"):
+        qr_group = generate_qr_code_display_group(pybadger, url)
+        pybadger.display.show(qr_group)
 
 
 class SocialBattery(DefaultMenuItemState):
